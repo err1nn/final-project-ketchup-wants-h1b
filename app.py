dab16f54df87a0a9bbbed02fddf97622e29252e3
kylieeeeee <kelly97491234@gmail.com>
2022-04-20 23:04:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ import pandas as pd
 import altair as alt
 from PIL import Image
 
+st.set_page_config(
+     page_title='H1B Data Visualization Application',
+     layout="wide",
+     initial_sidebar_state="expanded",
+     page_icon="🌐"
+)
+
 st.title("H1B data visualization")
 @st.cache  # add caching so we load the data only once
 
@@ -17,5 +24,27 @@ def load_data(url, encode = 'utf-8'):
 url_application = 'https://raw.githubusercontent.com/CMU-IDS-2022/final-project-ketchup-wants-h1b/main/Data/h1b_application.csv'
 url_employer = 'https://raw.githubusercontent.com/CMU-IDS-2022/final-project-ketchup-wants-h1b/main/Data/h1b_employers.csv'
 
-application_df = load_data(url_application)
-employer_df = load_data(url_employer)
+application = load_data(url_application)
+employer = load_data(url_employer)
+
+
+##Side Bar
+st.sidebar.header('Feature Selection:')
+
+major_list = application.FOREIGN_WORKER_INFO_MAJOR.value_counts()[application.FOREIGN_WORKER_INFO_MAJOR.value_counts()>100].index
+major_selection = st.sidebar.selectbox(
+    'Select or Type in Your Interested Major:',
+    major_list,
+)
+
+job_list = application.JOB_TITLE.value_counts()[application.JOB_TITLE.value_counts()>100].index
+job_selection = st.sidebar.selectbox(
+    'Select or Type in Your Interested Job Title:',
+    job_list
+)
+
+state_list = application.WORKSITE_STATE.unique()
+state_selection = st.sidebar.selectbox(
+    'Select or Type in Your Interested Worksite State:',
+    state_list
+)
